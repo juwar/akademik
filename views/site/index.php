b24952960a1defa028c334d6a14a89882c3a249b
Muhammad Faisal <muhammad.faisal.it-consultant@prudential.co.id>
2022-10-29 14:58:54 +0700

    add chart js

--- views/site/index.php
+++ views/site/index.php
@@ -2,51 +2,51 @@
 
 /** @var yii\web\View $this */
 
+use dosamigos\chartjs\ChartJs;
+
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+            <?= ChartJs::widget([
+                'type' => 'bar',
+                'options' => [
+                    'height' => 400,
+                    'width' => 400
+                ],
+                'data' => [
+                    'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+                    'datasets' => [
+                        [
+                            'label' => "My First dataset",
+                            'backgroundColor' => "rgba(179,181,198,0.2)",
+                            'borderColor' => "rgba(179,181,198,1)",
+                            'pointBackgroundColor' => "rgba(179,181,198,1)",
+                            'pointBorderColor' => "#fff",
+                            'pointHoverBackgroundColor' => "#fff",
+                            'pointHoverBorderColor' => "rgba(179,181,198,1)",
+                            'data' => [65, 59, 90, 81, 56, 55, 40]
+                        ],
+                        [
+                            'label' => "My Second dataset",
+                            'backgroundColor' => "rgba(255,99,132,0.2)",
+                            'borderColor' => "rgba(255,99,132,1)",
+                            'pointBackgroundColor' => "rgba(255,99,132,1)",
+                            'pointBorderColor' => "#fff",
+                            'pointHoverBackgroundColor' => "#fff",
+                            'pointHoverBorderColor' => "rgba(255,99,132,1)",
+                            'data' => [28, 48, 40, 19, 96, 27, 100]
+                        ]
+                    ]
+                ]
+            ]);
+            ?> 
         </div>
 
     </div>
